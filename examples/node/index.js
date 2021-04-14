1904b0a47ed6cf686c038cf47a54aea20f4da7d5
bogdanaksenov <bogdanaks@bk.ru>
2021-04-14 20:23:51 +0300

    Update examples

--- examples/node/index.js
+++ examples/node/index.js
@@ -1,11 +1,12 @@
-const NOWPaymentsApi = require('@nowpayments/nowpayments-api')
+const NOWPaymentsApi = require('@nowpayments/nowpayments-mass-payments-api-js')
 
-const NPApi = new NOWPaymentsApi({ apiKey: 'A7M40XV-CG1448Z-KVVED3G-NW3V0TK' })
+const NPApi = new NOWPaymentsApi({ apiKey: '4WHVF3X-E61MR3A-PM6HKW4-CBF7JVQ' })
 
 async function main() {
-  console.log('NOWPayments API - Example Node')
-  const { currencies } = await NPApi.getCurrencies()
-  console.log('Available currencies: ', currencies)
+  console.log('NOWPayments Mass Payments API - Example Node')
+  const { token } = await NPApi.auth({ email: 'kate.l@nowpayments.io', password: 'bogdan' })
+  const balance = await NPApi.getBalance({ token })
+  console.log('Get balance: ', balance)
 }
 
 main()
